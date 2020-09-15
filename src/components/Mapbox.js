57bd4f141284ef59764f8c569385fde0cdd3b0f3
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-09-15 15:18:22 -0400

    added map marker functionality being pulled from locations array

--- src/components/Mapbox.js
+++ src/components/Mapbox.js
@@ -11,6 +11,13 @@ const styles = {
   
 const Mapbox = ()=>{
     const mapContainer = useRef(null)
+    const locations=[
+      {lng:-73.920590,lat:40.766520},
+      {lng:-73.916630,lat:40.764300},
+      {lng:-73.931650,lat:40.772910},
+      {lng:-73.923120,lat:40.766110},
+      {lng:-73.916970,lat:40.77340},
+    ]
 
     useEffect(()=>{
         mapboxgl.accessToken = process.env.GATSBY_MAPBOX_ACCESS_TOKEN;
@@ -20,6 +27,11 @@ const Mapbox = ()=>{
             center: [-74, 40.72],
             zoom: 10.3
         })
+        locations.forEach(loc=>
+            new mapboxgl.Marker()
+                .setLngLat([loc.lng,loc.lat])
+                .addTo(map)
+        )
     },[])
 
 
