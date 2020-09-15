8b73b7a812aa1d0604dbeff997b810404783df28
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-09-15 18:33:28 -0400

    ran prettier on src

--- src/components/Mapbox.js
+++ src/components/Mapbox.js
@@ -1,50 +1,45 @@
-import React,{useEffect,useRef} from 'react'
-import mapboxgl from 'mapbox-gl'
-import "mapbox-gl/dist/mapbox-gl.css";
+import React, { useEffect, useRef } from "react"
+import mapboxgl from "mapbox-gl"
+import "mapbox-gl/dist/mapbox-gl.css"
 
 const styles = {
-    width: "50vw",
-    height: "calc(100vh - 80px)",
-    position: "absolute"
-  };
+  width: "50vw",
+  height: "calc(100vh - 80px)",
+  position: "absolute"
+}
 
-  
-const Mapbox = ()=>{
-    const mapContainer = useRef(null)
-    const locations=[
-      {lng:-73.920590,lat:40.766520},
-      {lng:-73.916630,lat:40.764300},
-      {lng:-73.931650,lat:40.772910},
-      {lng:-73.923120,lat:40.766110},
-      {lng:-73.916970,lat:40.77340},
-    ]
+const Mapbox = () => {
+  const mapContainer = useRef(null)
+  const locations = [
+    { lng: -73.92059, lat: 40.76652 },
+    { lng: -73.91663, lat: 40.7643 },
+    { lng: -73.93165, lat: 40.77291 },
+    { lng: -73.92312, lat: 40.76611 },
+    { lng: -73.91697, lat: 40.7734 }
+  ]
 
-    useEffect(()=>{
-        mapboxgl.accessToken = process.env.GATSBY_MAPBOX_ACCESS_TOKEN;
-        const map = new mapboxgl.Map({
-            container:mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [-74, 40.72],
-            zoom: 10.3
-        })
-        locations.forEach(loc=>{
-            const popup = new mapboxgl.Popup({ offset: 25 }).setText(
-                'This is a test location'
-                );
-            new mapboxgl.Marker()
-                .setLngLat([loc.lng,loc.lat])
-                .setPopup(popup)
-                .addTo(map)
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.GATSBY_MAPBOX_ACCESS_TOKEN
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-74, 40.72],
+      zoom: 10.3
     })
-    },[])
+    locations.forEach(loc => {
+      const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+        "This is a test location"
+      )
+      new mapboxgl.Marker()
+        .setLngLat([loc.lng, loc.lat])
+        .setPopup(popup)
+        .addTo(map)
+    })
+  }, [])
 
-
-
-    return(
-        <div 
-        ref={el => mapContainer.current = el} id='mapbox' 
-        style={styles}/>
-        )
+  return (
+    <div ref={el => (mapContainer.current = el)} id="mapbox" style={styles} />
+  )
 }
 
 export default Mapbox
