27d8d7b43657498113119464d133e08130a7092b
James Quigley <james@quigley.us>
2020-09-18 08:51:39 -0400

    Basic example of localization
    Change your browser langauge to see the difference.

---
+++ src/util/strings.js
@@ -0,0 +1,12 @@
+import LocalizedStrings from "react-localization"
+
+export const strings = new LocalizedStrings({
+  en: {
+    test: "Register to vote!",
+  },
+  es: {
+    test: "Registrarse para votar",
+  },
+})
+
+export default strings
